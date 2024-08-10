b6ebd1f300d1afa50b78350228fe59dd6b380f72
Alikadze <119809834+Alikadze@users.noreply.github.com>
2024-08-11 00:48:56 +0400

    Added pagination and project detail page

--- src/app/pages/project/project-with-id/project-with-id.component.ts
+++ src/app/pages/project/project-with-id/project-with-id.component.ts
@@ -1,23 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { ProjectFacade } from '../../../core/facades/project.facade';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProjectPayload } from '../../../core/interfaces/project';
 import { TranslateModule } from '@ngx-translate/core';
 import { DatePipe } from '@angular/common';
+import { ProjectCarouselComponent } from "../../../components/project-carousel/project-carousel.component";
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-project-with-id',
   standalone: true,
   imports: [
     TranslateModule,
-    DatePipe
-  ],
+    DatePipe,
+    ProjectCarouselComponent,
+    TagModule,
+    ButtonModule,
+],
   templateUrl: './project-with-id.component.html',
   styleUrl: './project-with-id.component.scss'
 })
 export class ProjectWithIdComponent {
   projectFacade = inject(ProjectFacade);
   private route = inject(ActivatedRoute);
+  router = inject(Router);
 
   project: ProjectPayload | undefined;
 
@@ -28,11 +35,19 @@ export class ProjectWithIdComponent {
     }
   }
 
+  navToContact() {
+    this.router.navigate(['/contact']);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
   loadProject(projectId: string) {
     this.projectFacade.getProjectById(projectId).subscribe(project => {
       this.project = project;
-      project.startDate = this.convertToDate(project.startDate);
-      project.endDate = this.convertToDate(project.endDate);
+      project.startDate = this.convertToDate(project.startDate as Date);
+      project.endDate = this.convertToDate(project.endDate as Date);
     });
   }
 
